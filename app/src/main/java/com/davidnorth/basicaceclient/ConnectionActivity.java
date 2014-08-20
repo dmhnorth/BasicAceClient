8f944266903d1fcc0673689f7346a03f08cd3dca
David North <dmhnorth@gmail.com>
2014-08-20 13:37:58 +0100

    Initial commit

---
+++ app/src/main/java/com/davidnorth/basicaceclient/ConnectionActivity.java
@@ -0,0 +1,61 @@
+package com.davidnorth.basicaceclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class ConnectionActivity extends Activity {
+
+        SimpleAndroidClient simpleAndroidClient = new SimpleAndroidClient();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection);
+
+        //TODO not allowed to run network connections in the UI thread
+        //http://www.androiddesignpatterns.com/2012/06/app-force-close-honeycomb-ics.html
+
+        simpleAndroidClient.launch();
+
+
+    }
+
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.connection, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void updateView(View view) {
+        TextView portOkay = (TextView) findViewById(R.id.status_line);
+        portOkay.setText(simpleAndroidClient.getResponseCode());
+
+        TextView getServerString = (TextView) findViewById(R.id.get_server_string);
+        getServerString.setText(simpleAndroidClient.getResponseString());
+
+    }
+}
---
+++ app/src/main/java/com/davidnorth/basicaceclient/SimpleAndroidClient.java
@@ -0,0 +1,61 @@
+package com.davidnorth.basicaceclient;
+
+import android.net.http.AndroidHttpClient;
+import android.util.Log;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * Created by Dave on 20/08/2014.
+ */
+public class SimpleAndroidClient {
+
+    //This is the address that can be used to access the emulator on a local host
+    private String localAddress = "http://10.0.2.2:4567";
+    private String responseCode = "currently unset";
+    private String responseString = "currently unset";
+
+
+    public void launch() {
+
+        Log.i("Launch:", "is running");
+        AndroidHttpClient androidHttpClient = AndroidHttpClient.newInstance("defaultAgentName");
+
+        //Recreates the curl, or browser call
+        HttpGet httpGet = new HttpGet(localAddress + "/hello");
+
+        //Now do the request
+        try {
+            //collect the response (This is the only line that does anything remotely)
+            HttpResponse response = androidHttpClient.execute(httpGet);
+            //do something with response
+
+
+            // 1. see if it connected worked by printing this to the log
+            responseCode = String.valueOf(response.getStatusLine().getStatusCode());
+            Log.i("Status Line:", String.valueOf(response.getStatusLine()));
+
+            // 2. use the response body to pull something out
+            responseString = EntityUtils.toString(response.getEntity());
+            Log.i("Get Entity:", String.valueOf(response.getEntity()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            androidHttpClient.close();
+        }
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public String getResponseString() {
+        return responseString;
+    }
+}
